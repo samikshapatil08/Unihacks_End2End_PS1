b082fc942403fd44ac405319f791d4f861cb0d30
Gauri Shirke <gaurishirke07@gmail.com>
2026-02-14 03:07:31 +0530

    debug false

--- reflect_comms/settings.py
+++ reflect_comms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pd2+595e4)2lfx1mrect29lux6b#a%eu4x43fme-nssvz9nbrp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
